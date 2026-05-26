73bc0eacc4cac11d57bd99ff64bf0f7fa371f69c
spessasus <spesekspesek@gmail.com>
2026-05-26 17:36:40 +0200

    update overlapping note test

--- tests/overlapping_notes_test.ts
+++ tests/overlapping_notes_test.ts
@@ -1,4 +1,5 @@
 import { MIDITestMaker } from "./test_maker";
+import { MIDIControllers } from "../src";
 
 const test = new MIDITestMaker("Overlapping Notes Test");
 
@@ -15,7 +16,23 @@ test.ticks += 480;
 test.noteOff(60);
 test.ticks += 480;
 test.noteOff(60);
+
+// Mono mode test
+test.ticks += 480;
+test.cc(MIDIControllers.monoModeOn, 0);
+test.noteOn(60, 127);
+test.ticks += 480;
+test.noteOn(64, 127);
+test.ticks += 480;
+test.noteOn(60, 127);
+test.ticks += 480;
+test.noteOff(60);
+test.ticks += 480;
+test.noteOn(60, 127);
+test.ticks += 480;
+test.noteOff(60);
+
+// End
 test.ticks += 1920;
 test.cc(1, 1);
-
 test.make();
