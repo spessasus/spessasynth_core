59aec83543d42c6c8945580555cbf0148316e785
spessasus <spesekspesek@gmail.com>
2026-08-05 12:43:11 +0200

    Add better test for the new MIDI Editor

--- tests/midi/midi_editor/log_events.ts
+++ tests/midi/midi_editor/log_events.ts
@@ -1,50 +1,58 @@
 import {
     MIDIControllers,
-    MIDIMessage,
+    BasicMIDI,
     MIDIMessageTypes,
     MIDIUtils
 } from "../../../src";
 import { arrayToHexString } from "../../../src/utils/other";
 
-export function logEventsTest(events: MIDIMessage[]) {
-    for (const event of events) {
-        switch (event.statusByte) {
-            case MIDIMessageTypes.controllerChange: {
-                console.info(
-                    "Controller change",
-                    (
-                        Object.keys(
-                            MIDIControllers
-                        ) as (keyof typeof MIDIControllers)[]
-                    ).find((k) => MIDIControllers[k] === event.data[0]),
-                    event.data[1]
-                );
-                break;
-            }
+export function logEventsTest(midi: BasicMIDI) {
+    console.group(`MIDI: ${midi.getName()}`);
+    for (const track of midi.tracks) {
+        console.group(`Track ${track.name}, events: ${track.events.length}`);
 
-            case MIDIMessageTypes.systemExclusive: {
-                const analyzed = MIDIUtils.analyzeSysEx(event.data);
-                for (const msg of analyzed) {
+        for (const event of track.events) {
+            switch (event.statusByte) {
+                case MIDIMessageTypes.controllerChange: {
                     console.info(
-                        "System Exclusive",
-                        Object.entries(msg)
-                            .map(([key, value]) => `${key}: ${value}`)
-                            .join(", ")
+                        "Controller change",
+                        (
+                            Object.keys(
+                                MIDIControllers
+                            ) as (keyof typeof MIDIControllers)[]
+                        ).find((k) => MIDIControllers[k] === event.data[0]),
+                        event.data[1]
+                    );
+                    break;
+                }
+
+                case MIDIMessageTypes.systemExclusive: {
+                    const analyzed = MIDIUtils.analyzeSysEx(event.data);
+                    for (const msg of analyzed) {
+                        console.info(
+                            "System Exclusive",
+                            Object.entries(msg)
+                                .map(([key, value]) => `${key}: ${value}`)
+                                .join(", ")
+                        );
+                    }
+                    break;
+                }
+
+                default: {
+                    console.info(
+                        (
+                            Object.keys(
+                                MIDIMessageTypes
+                            ) as (keyof typeof MIDIMessageTypes)[]
+                        ).find((k) => MIDIMessageTypes[k] === event.statusByte),
+                        arrayToHexString(event.data)
                     );
                 }
-                break;
-            }
-
-            default: {
-                console.info(
-                    (
-                        Object.keys(
-                            MIDIMessageTypes
-                        ) as (keyof typeof MIDIMessageTypes)[]
-                    ).find((k) => MIDIMessageTypes[k] === event.statusByte),
-                    arrayToHexString(event.data)
-                );
             }
         }
+        console.groupEnd();
     }
+
+    console.groupEnd();
 }
--- tests/midi/midi_editor/midi_editor_insertion_order.ts
+++ tests/midi/midi_editor/midi_editor_insertion_order.ts
@@ -1,14 +1,24 @@
+import {
+    MIDIControllers,
+    RegisteredParameterTypes,
+    SpessaLog
+} from "../../../src";
 import { MIDITestMaker } from "../../midi_file/midi_test_maker";
-import { MIDIControllers, MIDIMessage, SpessaLog } from "../../../src";
 import { logEventsTest } from "./log_events";
 
 const midi = new MIDITestMaker("MIDI Editor Insertion order", {
     system: "gm"
 });
 
-midi.note(50, 127).wait(480).note(64, 127);
+midi.note(50, 127)
+    .wait(480)
+    .rpn(RegisteredParameterTypes.fineTuning, 16_000)
+    .note(64, 127);
 
 midi.flush();
+
+console.info("\n\n\n--- BEFORE ---");
+logEventsTest(midi);
 
 SpessaLog.setLogLevel(true, true, true);
 
@@ -18,8 +28,8 @@ midi.modify({
         [
             0,
             {
+                fineTune: -40,
                 midiParams: {
-                    fineTune: -40,
                     modulationDepth: 40,
                     pitchWheel: 432,
                     pressure: 12
@@ -56,4 +66,5 @@ midi.modify({
         time: 64
     }
 });
-logEventsTest(midi.tracks[0].events as MIDIMessage[]);
+console.info("\n\n\n--- AFTER ---");
+logEventsTest(midi);
